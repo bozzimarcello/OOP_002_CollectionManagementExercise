c73767f40104919737d482f8faf59f61e5ab346b
Marcello Bozzi <marcello.bozzi@itismajo.onmicrosoft.com>
2024-02-28 08:46:30 +0100

    importato la soluzione del precedente esercizio

---
+++ BankConsoleApp/BankConsoleApp/Program.cs
@@ -0,0 +1,9 @@
+﻿using BankConsoleApp;
+
+BankAccount bankAccount = new BankAccount();
+
+bankAccount.Deposit(100);
+
+double balance = bankAccount.GetBalance();
+
+Console.WriteLine($"New balance: {balance}");
